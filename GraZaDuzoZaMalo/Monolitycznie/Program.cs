4b359fe0a63e893a822b236dcfd7729e86a90cec
morzeszko <55149644+morzeszko@users.noreply.github.com>
2019-09-11 20:25:20 +0200

    v1

---
+++ GraZaDuzoZaMalo/Monolitycznie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Komputer losuje liczbę z podanego zakresu
+            Random los = new Random();
+            int wylosowana = los.Next(1, 101);
+#if DEBUG
+            Console.WriteLine(wylosowana);
+#endif
+            // ----powtarzaj do odgadniecia
+            //2. Człowiek proponuje liczbę
+
+            
+            //3. Komputer ocenia
+            //----koniec powtarzania
+            //4. Wypisz statystyki gry
+        }
+    }
+}
